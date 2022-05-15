09e905450829896b90fa1c6cada3f86ac89648ff
wa_ab <waqas@itinsync.com>
2022-05-15 14:11:59 +0500

    Working Code, Conditions Fixed

--- java-wth-excel/src/main/java/ExelToJava.java
+++ java-wth-excel/src/main/java/ExelToJava.java
@@ -24,15 +24,15 @@ public class ExelToJava {
                 Date currentDate = new Date();
                 XSSFRow row = flightsSheet.getRow(rows.getRowNum());
                 var date = row.getCell(0).getDateCellValue();
-                if (currentDate.getDate() == date.getDate()) {
+                if (currentDate.getMonth() == date.getMonth() && currentDate.getDate() == date.getDate()) {
                     var percentOff = row.getCell(8).getRawValue().startsWith(String.valueOf(0))
                             ? Double.parseDouble(row.getCell(8).getRawValue()) : null;
                     if (percentOff != null && percentOff > 0.60 && percentOff < 0.99) {
                         var flight = new src.main.java.Flights();
                         flight.setId(UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE);
                         flight.setDate(row.getCell(0).getDateCellValue());
-                        flight.setOrigin(row.getCell(1).getRawValue());
-                        flight.setDestination(row.getCell(2).getRawValue());
+                        flight.setOrigin(row.getCell(1).getStringCellValue());
+                        flight.setDestination(row.getCell(2).getStringCellValue());
                         flight.setDealPrice(new Double(row.getCell(3).getNumericCellValue()).longValue());
                         flight.setNormalPrice(new Double(row.getCell(4).getNumericCellValue()).longValue());
                         flight.setDepartureDate(row.getCell(5).getDateCellValue());
@@ -40,18 +40,21 @@ public class ExelToJava {
                         flight.setNumberOfStops(new Double(row.getCell(7).getNumericCellValue()).longValue());
                         flight.setPercentOff(row.getCell(8).getNumericCellValue());
                         flight.setAirline(row.getCell(9).getStringCellValue());
-                        flight.setDepartureAirport(row.getCell(10).getRawValue());
-                        flight.setReturningAirport(row.getCell(11).getRawValue());
+                        flight.setDepartureAirport(row.getCell(10).getStringCellValue());
+                        flight.setReturningAirport(row.getCell(11).getStringCellValue());
                         flight.setGoodDeal(Boolean.parseBoolean(row.getCell(12).getStringCellValue()));
-                        flight.setLinkToFlights(row.getCell(13).getRawValue());
-                        flight.setSentToPremium(row.getCell(14).getRawValue());
-                        flight.setSentToFree(row.getCell(15).getRawValue());
-                        flight.setRemarks(row.getCell(16).getRawValue());
+                        flight.setLinkToFlights(row.getCell(13).getStringCellValue());
+                        flight.setSentToPremium(row.getCell(14).getStringCellValue());
+                        flight.setSentToFree(row.getCell(15).getStringCellValue());
+                        flight.setRemarks(row.getCell(16).getStringCellValue());
                         System.out.println(flight.getDate());
                         flights.add(flight);
                     }
                 }
             }
+        }
+        for (var flight : flights) {
+            System.out.println(flight.getOrigin() + " " + flight.getDestination() + " " + flight.getDate());
         }
     }
 }
